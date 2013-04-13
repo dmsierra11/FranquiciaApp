422cb0665b5ded3d7826346bea613eb079177216
Daniel <danieluchin@gmail.com>
2013-04-13 14:15:52 -0430

    segundo commit

---
+++ src/franquiciaapp/FranquiciaApp.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package franquiciaapp;
+
+/**
+ *
+ * @author daniel
+ */
+public class FranquiciaApp {
+    
+    private String ip_nodo_siguiente;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
