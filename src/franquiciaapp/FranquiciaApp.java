c915514428c984ad37fe6095e93e44087d22b105
GraLucena <Graciex.ls@gmail.com>
2013-04-15 00:33:19 -0430

    Revert "BorrarProductos"
    This reverts commit 2ca4e553653acf4484a89a7476177f428a35d9b2.

--- src/franquiciaapp/FranquiciaApp.java
+++ src/franquiciaapp/FranquiciaApp.java
@@ -12,7 +12,6 @@ public class FranquiciaApp {
 
     /**
      * @param args the command line arguments
-     * 
      */
     public static void main(String[] args) {
         // TODO code application logic here
--- src/franquiciaapp/XMLProducto.java
+++ src/franquiciaapp/XMLProducto.java
@@ -6,8 +6,6 @@ package franquiciaapp;
 
 import java.io.*;
 import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.jdom.*;
 import org.jdom.input.*;
 import org.jdom.output.*;
@@ -18,15 +16,7 @@ import org.jdom.output.*;
  */
 public class XMLProducto {
     
-        private Element root;
-
-    /**
-     * escribe en el XML los productos registrados en sistema
-     * @param nombre
-     * @param descripcion
-     * @param costo
-     * @return 
-     */
+    
     public static boolean agregarProducto(String nombre,String descripcion, String costo)
     {
         Document    doc;
@@ -87,107 +77,5 @@ public class XMLProducto {
         return true;
     }
     
-    //Listar productos 
-    /**
-     * Lista los productos que se encuentran en el .XML
-     * @param ventana
-     * @param archivo 
-     */
-    
-     public void listarProductos(GestionProducto ventana, String archivo) {
-        try {
-            SAXBuilder builder = new SAXBuilder(false);
-            //System.out.println(usuario);
-            Document doc = builder.build("listaProductos.xml");
-            Element raiz = doc.getRootElement();
-            List listaProducto = raiz.getChildren("producto");
-            Iterator k = listaProducto.iterator();
-            while (k.hasNext()) {
-                int i = 0, j = 0;
-                Element e = (Element) k.next();
-                Element nombre = e.getChild("nombre");
-                Element descripcion = e.getChild("descripcion");
-
-                Element costo = e.getChild("costo");
-               // if (archivo.equals(user.getText())) {
-                    ventana.agregarfila(nombre.getText(), descripcion.getText(), costo.getText(), costo.getText());
-                //}
-
-            }
-        } catch (FileNotFoundException F) {
-            System.out.println("Archivo XML no encontrado");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        //return Varchivo;
-    }
-     
-    /**
-     * Modifica el XML
-     * @return 
-     */
-    private boolean updateDocument() {
-        try {
-            XMLOutputter out = new XMLOutputter(org.jdom.output.Format.getPrettyFormat());
-            FileOutputStream file = new FileOutputStream("listaProductos.xml");
-            out.output(root, file);
-            file.flush();
-            file.close();
-            return true;
-        } catch (Exception e) {
-            System.out.println("error: " + e.getMessage());
-            return false;
-        }
-    }
-     
-    /**
-     * Borra el producto del XML
-     * @param archivo
-     * @param producto
-     * @return 
-     */
-    public boolean borrarProducto(String archivo, String producto) {
-        SAXBuilder builder = new SAXBuilder(false);
-            Document doc = null;
-        try {
-            doc = builder.build("listaProductos.xml");
-        } catch (JDOMException ex) {
-            Logger.getLogger(XMLProducto.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(XMLProducto.class.getName()).log(Level.SEVERE, null, ex);
-        }
-            root = doc.getRootElement(); 
-       boolean resultado = false;
-        Element aux = new Element("producto");
-        List Productos = this.root.getChildren("producto");
-        while (aux != null) {
-            aux = this.buscar(Productos, archivo, producto);
-            if (aux != null) {
-                Productos.remove(aux);
-                resultado = updateDocument();
-            }
-        }
-        return resultado;
-    }
-
-       /**
-        * Permite la busqueda en el XML
-        * @param raiz
-        * @param archivo
-        * @param producto
-        * @return 
-        */
-   public static Element buscar(List raiz, String archivo, String producto) {
-        Iterator i = raiz.iterator(); 
-        while (i.hasNext()) {
-            Element elemento = (Element) i.next();
-            if (producto.equals(elemento.getChild("nombre").getText()) ) {
-                return elemento;
-            }
-        }
-        return null;
-    }
-
-     
     
 }
