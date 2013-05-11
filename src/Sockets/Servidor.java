37d800e7fdf5ffdbdec94fa1abd424888305c4aa
GraLucena <Graciex.ls@gmail.com>
2013-05-11 02:01:33 -0430

    servidor

--- src/Sockets/Servidor.java
+++ src/Sockets/Servidor.java
@@ -11,20 +11,20 @@ import java.net.Socket;
  *
  * @author daniel
  */
-public class Servidor implements Runnable{
-    
+public class Servidor implements Runnable {
+
     private String puerto;
-    
-    public Servidor(String puerto){
+
+    public Servidor(String puerto) {
         this.puerto = puerto;
     }
-    
+
     @Override
-    public void run(){
+    public void run() {
         this.recibir();
     }
-    
-    public void recibir(){
+
+    public void recibir() {
         try {
             int bytesRead;
             // Creo el servicio y escucho por un puerto args[0]
@@ -39,17 +39,20 @@ public class Servidor implements Runnable{
                 socketServicio = servicio.accept();
 
                 DataInputStream in = new DataInputStream(socketServicio.getInputStream());
-                String fileName = in.readUTF();
-                OutputStream output = new FileOutputStream(fileName);
-                long size = in.readLong();
-                byte[] buffer = new byte[1024];
-                while (size > 0 && (bytesRead = in.read(buffer, 0, (int) Math.min(buffer.length, size))) != -1) {
-                    output.write(buffer, 0, bytesRead);
-                    size -= bytesRead;
+                String estado = in.readUTF();
+                if (!estado.equals("Estoy arriba")) {
+                    String fileName = in.readUTF();
+                    OutputStream output = new FileOutputStream(fileName);
+                    long size = in.readLong();
+                    byte[] buffer = new byte[1024];
+                    while (size > 0 && (bytesRead = in.read(buffer, 0, (int) Math.min(buffer.length, size))) != -1) {
+                        output.write(buffer, 0, bytesRead);
+                        size -= bytesRead;
+                    }
+                    output.close();
+                } else {
+                    System.out.println(estado);
                 }
-
-                output.close();
-
                 socketServicio.close();
             }
 
